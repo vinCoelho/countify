911e16b77150ff7db600d4bada42894c4694e857
Vinícius Coelho <viniciusraposo@gmail.com>
2023-02-13 13:43:33 -0300

    update

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -32,19 +32,27 @@ function Countdown() {
 
   return (
     <div className='lg:mr-10 p-6 md:p-10 mb-8 md:mb-0 bg-[#f1fa8c] bg-opacity-80 rounded-2xl flex flex-col items-center'>
-      <div>
-        <h1 className='md:text-4xl text-3xl text-center mb-4 lg:mb-6 font-mono'>୵୵countify</h1>
-        <input className='text-center text-4xl lg:text-6xl w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 pl-4 pr-1 md:pl-7 md:pr-4 font-mono text-[#ff79c6] bg-gray-900 rounded-2xl'
-          type="number"
-          value={minutes}
-          onChange={e => setMinutes(parseInt(e.target.value))}
-        />
-        <span className='text-5xl lg:text-7xl md:text-6xl'> : </span>
-        <input className='text-center text-4xl lg:text-6xl w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 pl-4 pr-1 md:pl-7 md:pr-4 font-mono text-[#ff79c6] bg-gray-900 rounded-2xl'
-          type="number"
-          value={seconds}
-          onChange={e => setSeconds(parseInt(e.target.value))}
-        />
+      <h1 className='md:text-4xl text-3xl text-center mb-4 lg:mb-6 font-mono'>୵୵countify</h1>
+      <div className='flex items-center justify-center'>
+        <article className='flex flex-col w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 rounded-2xl bg-gray-900 items-center justify-center'>
+          <input 
+            className='ml-3 text-center text-4xl lg:text-6xl lg:h-20 lg:w-24 h-12 w-16 font-mono text-[#ff79c6] bg-transparent'
+            type="number"
+            value={minutes}
+            onChange={e => setMinutes(parseInt(e.target.value))}
+          />
+          <p className='text-gray-500 font-mono text-sm'>minutes</p>
+        </article>
+          <span className='animate-pulse px-1 lg:mb-3 text-5xl lg:text-7xl md:text-6xl'> : </span>
+        <article className='flex flex-col w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 rounded-2xl bg-gray-900 items-center justify-center'>
+          <input 
+            className='ml-3 text-center text-4xl lg:text-6xl lg:h-20 lg:w-24 h-12 w-16 font-mono text-[#ff79c6] bg-transparent'
+            type="number"
+            value={seconds}
+            onChange={e => setSeconds(parseInt(e.target.value))}
+          />
+          <p className='text-gray-500 font-mono text-sm'>seconds</p>
+        </article>
       </div>
       <div className='flex items-center flex-col md:flex-col lg:flex-row'>
         <h6 className='font-mono px-2 pt-4 lg:pt-6 text-[#282a36] font-extrabold'>⚙ cntrls</h6>
