dd11904292ac7f0676e04d584e92c728d0ed8ede
Vinícius Coelho <viniciusraposo@gmail.com>
2023-02-15 16:54:42 -0300

    randomizing the background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,57 @@
+import { useEffect, useState } from "react";
 import Countdown from "./components/Countdown";
 import Player from "./components/Player";
+import city from "/src/img/city.jpg";
+import city2 from "/src/img/city-2.jpg";
+import girl from "/src/img/girl.jpg";
+import lofi from "/src/img/lofi.jpg";
+import lonelyMan from "/src/img/lonely-man.jpg";
+import neonCity from "/src/img/neon-city.jpg";
+import sadGirl from "/src/img/sad-girl.jpg";
+import sky from "/src/img/sky.jpg";
+import space from "/src/img/space.jpg";
+import Footer from "./components/Footer";
 
-export default function App() {
+function App() {
+  const [bg, setBg] = useState("");
+
+  const randomBg = () => {
+    const bgArr = [
+      city,
+      city2,
+      girl,
+      lofi,
+      lonelyMan,
+      neonCity,
+      sadGirl,
+      sky,
+      space,
+    ];
+    const random = bgArr[Math.floor(Math.random() * bgArr.length)];
+    const randomRes = random;
+    const bg = randomRes.slice(9);
+    setBg(bg);
+  };
+
+  useEffect(() => {
+    randomBg();
+  }, []);
+
   return (
-    <div className='m-auto w-screen lg:max-w-6xl h-screen flex md:flex-row flex-col items-center justify-center'>
-      <Countdown />
-      <Player />
+    <div className="w-screen"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="m-auto w-screen lg:max-w-6xl h-screen flex md:flex-row flex-col items-center justify-center">
+        <Countdown />
+        <Player />
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
+
+export default App;
--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,38 +1,65 @@
-import { useEffect, useState } from 'react';
-import { getPlaylist } from '../service/api';
+import { useEffect, useState } from "react";
+import { getPlaylist } from "../service/api";
 
 function Player() {
-  const [html, setHtml] = useState('');
-  const [input, setInput] = useState<string>('');
+  const [html, setHtml] = useState("");
+  const [input, setInput] = useState<string>("");
+  const [load, setLoad] = useState(false);
 
   const getList = async () => {
-    const retrievedUrl = localStorage.getItem('savedUrl');
-    console.log(retrievedUrl);
-    
-    const response = await getPlaylist(
-      input.length > 0 ? input 
-      : retrievedUrl
-      || 'https://open.spotify.com/playlist/739UpZIZldjAMBSiAZjz1q?si=4e351f648aa348b3')
-    setHtml(response.html)
+    const retrievedUrl = localStorage.getItem("savedUrl");
+    setLoad(true);
 
-    input && localStorage.setItem('savedUrl', input)
-    setInput('');
-  }
+    const response = await getPlaylist(
+      input.length > 0
+        ? input
+        : retrievedUrl ||
+            "https://open.spotify.com/playlist/739UpZIZldjAMBSiAZjz1q?si=4e351f648aa348b3"
+    );
+    setHtml(response.html);
+    setLoad(false);
+
+    input && localStorage.setItem("savedUrl", input);
+    setInput("");
+  };
 
   useEffect(() => {
-    getList()
-  }, [])
+    getList();
+  }, []);
 
   return (
-    <div className='w-1/2 text-center flex flex-col sm:items-center'>
-      <section className='flex justify-between'>
-        <input className='md:w-auto min-w-0 bg-[#44475a] bg-opacity-50 text-white py-1 px-2 border-2 border-[#ff79c6] font-mono rounded-lg mb-8' type='text' value={input} placeholder='insert a url...' onChange={({ target }: { target: HTMLInputElement }) => setInput(target.value)} />
-        <button className='w-38 h-9 py-1.5 px-4 ml-4 font-mono bg-[#ff79c6] rounded-lg text-white disabled:opacity-70' disabled={ input.length <= 24 } onClick={getList}>
-          Play!
+    <div className="w-1/2 text-center flex flex-col sm:items-center">
+      <section className="flex justify-between">
+        <input
+          className="md:w-auto min-w-0 bg-[#44475a] bg-opacity-80 text-white py-1 px-2 border-2 border-[#ff79c6] font-mono rounded-lg mb-4"
+          type="text"
+          value={input}
+          placeholder="insert a url..."
+          onChange={({ target }: { target: HTMLInputElement }) =>
+            setInput(target.value)
+          }
+        />
+        <button
+          className="w-38 h-9 py-1.5 px-4 ml-4 font-mono bg-[#ff79c6] rounded-lg text-white disabled:opacity-70"
+          disabled={input.length <= 24}
+          onClick={getList}
+        >
+          go!
         </button>
       </section>
-      { html && <div className='opacity-90' dangerouslySetInnerHTML={{ __html: html }} /> }
-      <h3 className='w-auto max-w-xs bg-[#ff79c6]/70 text-white text-xs rounded-2xl px-3 py-2 font-mono mt-4'>Faça login no Spotify para reproduzir as músicas na íntegra.</h3>
+      {load ? (
+        <p className="text-white font-mono">Loading...</p>
+      ) : (
+        html && (
+          <div
+            className="opacity-90"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        )
+      )}
+      <h3 className="sm:w-72 w-auto max-w-xs bg-[#ff79c6]/80 text-white text-xs rounded-2xl px-3 py-2 font-mono mt-4">
+        Faça login no Spotify para reproduzir as músicas na íntegra.
+      </h3>
     </div>
   );
 }
