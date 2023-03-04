9a181f23db3c41591bd1bdd7f7cf3901e5320ae4
Vinícius Coelho <viniciusraposo@gmail.com>
2023-03-04 14:46:04 -0300

    new version of the app with the stop music feature

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Provider } from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <App />
+)
